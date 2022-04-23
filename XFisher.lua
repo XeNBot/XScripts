552ab0e51099faadfffecbf60d5a7ea9d70dacee
unknown <57272096+MarioJimenezZ@users.noreply.github.com>
2022-04-23 10:33:31 -0700

    Updated API

--- XFisher.lua
+++ XFisher.lua
@@ -5,11 +5,26 @@ local XFisher = Class("XFisher")
 function XFisher:initialize()
 	
 	self.running = false
+	-- counter for fishes caught
 	self.fishesCaught = 0
+
+	-- new way of using actions
+	self.actions = {
+
+		cast    = Action(1, 289),
+		hook    = Action(1, 296),
+		mooch   = Action(1, 297),
+		chum    = Action(1, 4104),
+		thaliak = Action(1, 26804)
+
+	}
 
 	self.menu = Menu("XFisher")
 
 	self.menu:label("Walk to fishing spot and press start")
+	self.menu:checkbox("Use Chum", "USE_CHUM", true)
+	self.menu:checkbox("Use Mooch", "USE_MOOCH", true)
+	self.menu:checkbox("Use Thaliak's Favor", "USE_THALIAK", true)
 	self.menu:space()
 	self.menu:button("Start", "BTN_START", function()
 		if self.running then
@@ -24,16 +39,36 @@ function XFisher:initialize()
 	
 	Callbacks:Add(CALLBACK_FISH_BITE, function(marks)
 		-- Uses Hook whenever a fish bites
-		print("Fish just bit and has " .. marks .. " marks")
-		ActionManager.UseAction(1, 296, TARGET_INVALID_ID)
-		Keyboard.SendKey(96)
+		print("Got a bite with " .. marks .. " mark(s)")
+		self.actions.hook:use()
 	end)
 
 	Callbacks:Add(CALLBACK_FISH_CATCH, function(fishId)
 		-- Prints the fish id of any fishes we catch
 		self.fishesCaught = self.fishesCaught + 1
-		print("New Fish Cought:", fishId, "Fishes Caught So Far: ", self.fishesCaught)
-		ActionManager.UseAction(1, 299, TARGET_INVALID_ID) -- Quits and restarts
+		print("[".. os.date( "!%a %b %d, %H:%M", os.time() - 7 * 60 * 60 ) .. "]: Caught a fish! id : " ..  fishId ..", So far we caught : " ..  self.fishesCaught .. " fishes")
+		-- Can Mooch?
+		if self.menu["USE_MOOCH"].bool and self.actions.mooch:canUse() then
+			print("Using fish with mooch to catch bigger one!")
+			self.actions.mooch:use()
+		elseif self.menu["USE_CHUM"].bool and self.actions.chum:canUse() then
+			self.actions.chum:use()
+		else
+			self.actions.cast:use()
+		end
+	end)
+
+	Callbacks:Add(CALLBACK_FISH_FAIL, function()
+		print("Oh noes, the fish got away! trying again")
+		self.actions.cast:use()
+	end)
+
+	Callbacks:Add(CALLBACK_ACTION_USED, function(type, id, targetId)
+		-- Chum id
+		if id == self.actions.chum.id then
+			print("Used Chum, time to fish!")
+			self.actions.cast:use()
+		end
 	end)
 
 	Callbacks:Add(CALLBACK_PLAYER_TICK, function() self:Tick() end)
@@ -42,21 +77,22 @@ function XFisher:initialize()
 end
 
 function XFisher:Tick()
-	if player.isCasting or not self.running then return end -- prevents actions while casting or not running
+	if not self.running then return end -- prevents actions while casting or not running
+
+	if self.menu["USE_THALIAK"].bool and self.actions.thaliak:canUse() and (player.maxGP - player.GP) > 150 then
+		self.actions.thaliak:use()
+		return
+	end
+
 	-- Checks if we're fishing
 	if not player.isFishing then
+		-- Checks if we have Chum on
+		if self.menu["USE_CHUM"].bool and self.actions.chum:canUse() then
+			self.actions.chum:use()
 		-- Check if we can use the fish action (if we're in a fishing area) and if we're not already casting
-		if ActionManager.CanUseAction(1, 289, TARGET_INVALID_ID) then
-			-- uses action
-			ActionManager.UseAction(1, 289, TARGET_INVALID_ID)
-		end
-	else
-		-- we're fishing
-		if player.fishingStatus == FISHING_WAITING then 
-			if ActionManager.CanUseAction(1, 289, TARGET_INVALID_ID) then
-				ActionManager.UseAction(1, 289, TARGET_INVALID_ID) -- Quits and restarts
-			end
-		end
+		elseif self.actions.cast:canUse() then
+			self.actions.cast:use()
+		end	
 	end
 end
 
