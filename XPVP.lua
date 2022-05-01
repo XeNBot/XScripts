e0bb8c8ba3fc84465c0419063b74a27aa6754839
unknown <57272096+MarioJimenezZ@users.noreply.github.com>
2022-04-30 19:37:25 -0700

    Reaper update
    	- Added Reaper class support
    	- Uses different combo rotations for best situations
    	- Uses soul sacrifice stacks properly
    	- Minimum stack setter for plentiful harvest
    	- Uses Arcane Crest to auto heal
    	- Gap closers & initiations

--- XPVP.lua
+++ XPVP.lua
@@ -47,6 +47,18 @@ function XPVP:initMenu()
 				self.menu["ACTIONS"]["SAM"]:checkbox("Use Meikyo Shisui",       "MEI", true)
 				self.menu["ACTIONS"]["SAM"]:checkbox("Only Use Shisui When CC", "MEI_CC", true)
 				self.menu["ACTIONS"]["SAM"]:checkbox("Use Zantetsuken",         "ZANTET", true)
+
+			self.menu["ACTIONS"]:subMenu("Reaper", "RPR")
+				self.menu["ACTIONS"]["RPR"]:checkbox("Use Infernal Slice Combo", "INFERNAL", true)
+				self.menu["ACTIONS"]["RPR"]:checkbox("Use Soul Slice",           "SOUL",  true)
+				self.menu["ACTIONS"]["RPR"]:checkbox("Use Plentyful Harvest",    "HARVEST", true)
+				self.menu["ACTIONS"]["RPR"]:number("Min Soul Sacrifice Stacks",  "HARVEST_MIN", 6)
+				self.menu["ACTIONS"]["RPR"]:checkbox("Use Grim Swathe",          "GRIM", true)
+				self.menu["ACTIONS"]["RPR"]:checkbox("Use Death Warrant",        "DEATH", true)
+				self.menu["ACTIONS"]["RPR"]:checkbox("Hell's Ingress",           "HELL", true)
+				self.menu["ACTIONS"]["RPR"]:checkbox("Arcane Crest",             "ARCANE", true)
+				self.menu["ACTIONS"]["RPR"]:checkbox("Use Tenebrae Lemurum",     "TENEBRAE", true)
+
 		
 		self.menu:subMenu("Extra Settings", "EXTRA")
 			self.menu["EXTRA"]:checkbox("Practice Combo Dummies", "PRACTICE", true)
@@ -125,7 +137,32 @@ function XPVP:initialize()
 			wildfire   = Action(1, 29409),
 			bishop     = Action(1, 29412),
 			analysis   = Action(1, 29414),
-			spite      = Action(1, 29415)
+			spite      = Action(1, 29415),
+		},
+
+		reaper = {
+			
+			-- Infernal Slice Combo
+			slice    = Action(1, 29538),
+			waxing   = Action(1, 29539),
+			infernal = Action(1, 29540),
+			gibbet   = Action(1, 29541),
+			gallows  = Action(1, 29542),
+			void     = Action(1, 29543),
+			cross    = Action(1, 29544),
+
+			soul     = Action(1, 29566),
+			harvest  = Action(1, 29546),
+			grim     = Action(1, 29547),
+			death    = Action(1, 29549),
+			hell     = Action(1, 29550),
+			arcane   = Action(1, 29552),
+			tenebrae = Action(1, 29553),
+
+
+
+
+
 		}
 
 	}
@@ -170,6 +207,8 @@ function XPVP:Tick()
 			self:DragoonCombo()
 		elseif player.classJob == 31 then
 			self:MachinistCombo()
+		elseif player.classJob == 39 then
+			self:ReaperCombo()
 		end
 	end
 end
@@ -208,6 +247,10 @@ function XPVP:CommonActions()
 	elseif menu["PURIFY"].bool and self:ShouldPurify() and actions.purify:canUse(self.none) then
 		actions.purify:use(self.none)
 		return true
+	elseif player.classJob == 39 and self.menu["ACTIONS"]["RPR"]["ARCANE"].bool and (player.maxHealth - player.health) > 18000 and 
+		self.actions.reaper.arcane:canUse(self.none) then
+		self.actions.reaper.arcane:use(self.none)
+		return true
 	elseif menu["RECUPERATE"].bool and (player.maxHealth - player.health) > 15000 and actions.recuperate:canUse(self.none) then
 		actions.recuperate:use(self.none)
 		return true
@@ -228,6 +271,70 @@ function XPVP:ShouldPurify()
 				return false
 			end
 			return true
+		end
+	end
+
+end
+
+function XPVP:ReaperEnshrouded(target, status, actions)
+	-- Communio
+	if (status.remainingTime <= 1.5 or status.count == 1) and actions.tenebrae:canUse(target.id) then
+		actions.tenebrae:use(target.id)
+	-- Lemure's Slice
+	elseif actions.grim:canUse(target.id) then
+		actions.grim:use(target.id)
+	-- Void Reaping
+	elseif actions.void:canUse(target.id) then
+		actions.void:use(target.id)
+	-- Cross Reaping
+	elseif actions.cross:canUse(target.id) then
+		actions.cross:use(target.id)
+	elseif actions.death:canUse(target.id) then
+		actions.death:use(target.id)
+	end
+end
+
+function XPVP:ReaperCombo()
+	local menu    = self.menu["ACTIONS"]["RPR"]
+	local actions = self.actions.reaper
+
+	local farTarget = self:GetTarget(14)
+
+	if farTarget ~= nil and farTarget.valid and farTarget.pos:dist(player.pos) > 9 and menu["HELL"].bool and actions.hell:canUse(farTarget.id) and not player:hasStatus(2860) then
+		player:rotateTo(farTarget.pos)
+		actions.hell:use(farTarget.id)
+		return
+	end
+
+	local target = self:GetTarget(5)
+
+	if target ~= nil and target.valid then
+
+		local enshrouded     = player:getStatus(2863)
+		local soul_sacrifice = player:getStatus(3204)
+
+		if enshrouded.valid then 
+			self:ReaperEnshrouded(target, enshrouded, actions)
+		elseif menu["TENEBRAE"].bool and actions.tenebrae:canUse(self.none) then
+			actions.tenebrae:use(self.none)
+		elseif menu["DEATH"].bool and actions.death:canUse(target.id) then
+			actions.death:use(target.id)
+		elseif menu["HARVEST"].bool and actions.harvest:canUse(target.id) and soul_sacrifice.valid and soul_sacrifice.count >= menu["HARVEST_MIN"].int then
+			actions.harvest:use(target.id)
+		elseif menu["SOUL"].bool and actions.soul:canUse(target.id) and (not soul_sacrifice.valid or (soul_sacrifice.count < 8)) then
+			actions.soul:use(target.id)
+		elseif menu["GRIM"].bool and actions.grim:canUse(target.id) then
+			actions.grim:use(target.id)
+		elseif menu["INFERNAL"].bool and actions.gibbet:canUse(target.id) then
+			actions.gibbet:use(target.id)
+		elseif menu["INFERNAL"].bool and actions.gallows:canUse(target.id) then
+			actions.gallows:use(target.id)
+		elseif menu["INFERNAL"].bool and actions.infernal:canUse(target.id) then
+			actions.infernal:use(target.id)
+		elseif menu["INFERNAL"].bool and actions.waxing:canUse(target.id) then
+			actions.waxing:use(target.id)
+		elseif menu["INFERNAL"].bool and actions.slice:canUse(target.id) then
+			actions.slice:use(target.id)
 		end
 	end
 
